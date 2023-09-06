8c2502b634f46343afd777ec72b386b41c91b898
Ryo Konishi <ryoestrella@gmail.com>
2023-09-06 23:14:11 +0200

    no message

--- src/locations/Sidebar/Sidebar.tsx
+++ src/locations/Sidebar/Sidebar.tsx
@@ -1,18 +1,27 @@
 import { useState, useEffect } from 'react';
 import { SidebarAppSDK } from '@contentful/app-sdk';
-import { Paragraph, Button, Card, Stack, Flex, ButtonGroup, ToggleButton } from '@contentful/f36-components';
+import { Paragraph, Button, Card, Stack, Flex, ButtonGroup, ToggleButton, Checkbox, IconButton } from '@contentful/f36-components';
 import { CopyIcon, DoneIcon, CloudUploadIcon } from '@contentful/f36-icons';
 import { /* useCMA, */ useSDK } from '@contentful/react-apps-toolkit';
 import { styles } from './Sidebar.styles';
+
+type thumbnailType = {
+  url: string;
+  filename: string;
+};
 
 const Sidebar = () => {
   const sdk = useSDK<SidebarAppSDK>();
   const {
     copyFormat
   } = sdk.parameters.installation
-  const [thumbnails, setThumbnails] = useState([]);
+  const [thumbnails, setThumbnails] = useState<thumbnailType[]>([]);
   const [copiedUrl, setCopiedUrl] = useState({copied: ""});
   const [copyStyle, setCopyStyle] = useState("markdown");
+  const [copiedAllUrl, setCopiedAllUrl] = useState(false);
+  const userId = sdk.user.sys.id
+  const entrySys = sdk.entry.getSys()
+  const entryId = entrySys.id
 
   const openDialog = async () => {
     sdk.dialogs
@@ -21,22 +30,41 @@ const Sidebar = () => {
       minHeight: 640,
       position: 'center',
       shouldCloseOnOverlayClick: true,
-      shouldCloseOnEscapePress: true
+      shouldCloseOnEscapePress: true,
+      parameters: { userId: userId || "nodata", entryId: entryId || "nodata" }
       })
     .then((data) => {
-      setThumbnails(data) 
+      data.map((image: any) => {
+        setThumbnails((prevThumbnails) => {
+          return [...prevThumbnails, image];
+        });
+      })
+      setCopiedAllUrl(false)
     });
   }
 
-  const copyAssetToClipboard = async (url: string, filename: string) => {
+  const returnFormattedAsset = (url: string, filename: string) => {
     let formatText = url
     if(copyStyle === "markdown"){
       formatText = `![${filename}](${url})`
     }else if(copyStyle === "html"){
       formatText = `<img src="${url}" alt="${filename}" />`
     }
-    await navigator.clipboard.writeText(formatText || "")
+    return formatText
+  };
+
+  const copyAssetToClipboard = async (url: string, filename: string) => {
+    const target = returnFormattedAsset(url, filename)
+    await navigator.clipboard.writeText(target || "")
     setCopiedUrl({copied: url})
+  };
+
+  const copyAllToClipboard = async () => {
+    const allTargets = thumbnails.map(({url, filename}) => {
+      return returnFormattedAsset(url, filename)
+    })
+    await navigator.clipboard.writeText(allTargets.join("\r")|| "")
+    setCopiedAllUrl(true)
   };
 
   useEffect(() => {
@@ -71,9 +99,15 @@ const Sidebar = () => {
           ))}
             </ButtonGroup>
           }
+        
+          <Button
+            size="small"
+            startIcon={copiedAllUrl ? <DoneIcon /> : <CopyIcon />}
+            onClick={() => copyAllToClipboard()}    
+          >Copy ALL</Button>
+
           {thumbnails.map(({url, filename}) => (
           <Card
-            onClick={() => copyAssetToClipboard(url, filename)}
             key={url}
             className={styles.thumbnail}
           >
@@ -81,19 +115,37 @@ const Sidebar = () => {
               flexDirection="row"
               gap="spacingXs"
               alignItems="center"
+              justifyContent="space-between"
             >
-              <img src={url} />
+              <Flex
+                alignItems="center"
+                gap="spacingXs"
+              >
+                <img src={url} />
+
                 <div className={styles.copyTexts}>
-                  <p className={styles.copyClip}>{copiedUrl["copied"] === url ? "Copied!" : "Copy to Clipboard"}</p>
                   <p className={styles.fileName}>{filename}</p>
                 </div>
+              </Flex>
                 
-                <div className={styles.copyIcon}>
+              <div className={styles.copyIcon}>
                 {copiedUrl["copied"] === url
-                  ? <DoneIcon size="tiny" variant="secondary" />
-                  : <CopyIcon size="tiny" variant="secondary" />
+                  ? <IconButton
+                    variant="transparent"
+                    aria-label="Copy URL"
+                    size="small"
+                    icon={<DoneIcon />}
+                    />
+                  : <IconButton
+                    variant="secondary"
+                    aria-label="Copy URL"
+                    size="small"
+                    icon={<CopyIcon />}
+                    onClick={() => copyAssetToClipboard(url, filename)}
+                    />
                 }
-                </div>
+              </div>
+
             </Flex>
           </Card>
           ))}
